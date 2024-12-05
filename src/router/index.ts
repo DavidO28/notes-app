29804ec4f91ea0b3ec8d0322a9b2c3e40f24ee63
DavidO28 <damers22@yahoo.com>
2024-12-05 17:02:27 +0400

    updated configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/notes-home',
+      path: '/notes-app',
       name: 'home',
       component: HomeView,
     },
