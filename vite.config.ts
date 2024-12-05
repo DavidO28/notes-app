534487298756b7fa59c232d867c31a0cb2a87b29
DavidO28 <damers22@yahoo.com>
2024-12-05 17:00:31 +0400

    added configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/notes-home',
+  base: process.env.NODE_ENV === 'production' ? '/notes-app/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
